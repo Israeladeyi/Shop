590d5b3a6e767c259ada737f9d82d3383f37ba8d
Adeyi Israel <adeyiisreal3@gmail.com>
2022-12-09 13:25:14 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,24 +3,24 @@ let shopitemData = [{
     name: 'Gucci',
     price:7000,
     desc: 'Lorem ipsum is a music artist in the world who is a ',
-    img: '/Images/img-1.jpg',
+    img: '/Images/Img-1.jpg',
 }, {
     id: 'item2',
     name: 'louis vuitton ',
     price: 9000,
     desc: 'Lorem ipsum is a music artist in the world who is a ',
-    img: '/Images/img-2.jpg',
+    img: '/Images/Img-2.jpg',
 }, {
     id: 'item3',
     name: 'Prada',
     price: 3000,
     desc: 'Lorem ipsum is a music artist in the world who is a ',
-    img: '/Images/img-3.jpg',
+    img: '/Images/Img-3.jpg',
 }, {
     id: 'item4',
     name: 'Versace',
     price: 5000,
     desc: 'Lorem ipsum is a music artist in the world who is a ',
-    img: '/Images/img-4.jpg',
+    img: '/Images/Img-4.jpg',
 }]
 
