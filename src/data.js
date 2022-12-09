39a278e854380014d7acb9c8cdd865f3e9255e2f
Adeyi Israel <adeyiisreal3@gmail.com>
2022-12-09 13:10:02 +0100

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+let shopitemData = [{
+    id: 'item1',
+    name: 'Gucci',
+    price:7000,
+    desc: 'Lorem ipsum is a music artist in the world who is a ',
+    img: 'Images/img-1.jpg',
+}, {
+    id: 'item2',
+    name: 'louis vuitton ',
+    price: 9000,
+    desc: 'Lorem ipsum is a music artist in the world who is a ',
+    img: 'Images/img-2.jpg',
+}, {
+    id: 'item3',
+    name: 'Prada',
+    price: 3000,
+    desc: 'Lorem ipsum is a music artist in the world who is a ',
+    img: 'Images/img-3.jpg',
+}, {
+    id: 'item4',
+    name: 'Versace',
+    price: 5000,
+    desc: 'Lorem ipsum is a music artist in the world who is a ',
+    img: 'Images/img-4.jpg',
+}]
+
